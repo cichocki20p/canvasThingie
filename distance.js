51ad25942cf5466a7d3ef61d16915072ba1176b6
cichocki20p <113165962+cichocki20p@users.noreply.github.com>
2022-09-27 22:41:48 +0200

    added borders + little changes

--- distance.js
+++ distance.js
@@ -4,7 +4,16 @@ function distance(x1, y1, x2, y2) {
     return Math.sqrt((a * a) + (b * b))
 }
 
+function distance2(x1, y1, x2, y2) {
+    return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2)) 
+}
+
 function collide(rad1, rad2, distance) {
     if ((rad1 + rad2) > distance) return true
+    return false
+}
+
+function isColliding(obj1, obj2, x, y) {
+    if (collide(obj1.size, obj2.size, distance(obj1.x + x, obj1.y + y, obj2.x, obj2.y))) return true
     return false
 }
--- script2.js
+++ script2.js
@@ -7,11 +7,15 @@ canvas.height = innerHeight - 100
 let midOS = [canvas.width / 2, canvas.height / 2]
 
 const player = new Player(midOS[0], midOS[1], 20, "aqua", 10)
-// player.draw()
 const rock = new GameObject(midOS[0] - 100, midOS[1] - 100, 40, "red")
-// rock.draw()
+const rock2 = new GameObject(midOS[0] + 100, midOS[1] + 200, 40, "red")
+const rock3 = new GameObject(midOS[0] - 300, midOS[1] - 180, 40, "red")
+const rock4 = new GameObject(midOS[0] - 100, midOS[1] + 300, 40, "red")
+const rock5 = new GameObject(midOS[0] + 300, midOS[1] + 80, 40, "red")
+const rock6 = new GameObject(midOS[0] - 90, midOS[1] + 250, 40, "red")
 
-let x = distance(player.x, player.y, rock.x, rock.y)
+let x = distance2(player.x, player.y, rock.x, rock.y)
+
 
 let moveLeft = false
 let moveRight = false
@@ -56,10 +60,33 @@ function clearScreen() {
     c.clearRect(0, 0, canvas.width, canvas.height)
 }
 
+function borderContact(obj) {
+    if ((obj.x + obj.size) > canvas.width ||
+        (obj.x - obj.size) < 0 ||
+        (obj.y + obj.size) > canvas.height ||
+        (obj.y - obj.size) < 0) {
+        return true
+    } return false
+}
+
 function gameLoop() {
 
-    x = distance(player.x, player.y, rock.x, rock.y)
-    if (collide(player.size, rock.size, x)) {
+    // x = distance2(player.x, player.y, rock.x, rock.y)
+    // if (collide(player.size, rock.size, x)) {
+    //     player.color = "yellow"
+    // } else {
+    //     player.color = "aqua"
+    // }
+    let collision = (
+        isColliding(player, rock, 0, 0) || 
+        isColliding(player, rock2, 0, 0) || 
+        isColliding(player, rock3, 0, 0) ||
+        isColliding(player, rock4, 0, 0) ||
+        isColliding(player, rock5, 0, 0) ||
+        isColliding(player, rock6, 0, 0)
+    )
+
+    if (collision) {
         player.color = "yellow"
     } else {
         player.color = "aqua"
@@ -68,24 +95,41 @@ function gameLoop() {
     if (moveLeft) {
         clearScreen()
         player.moveLeft()
+        if (borderContact(player)) {
+            player.moveRight()
+        }
     }
 
     if (moveRight) {
         clearScreen()
         player.moveRight()
+        if (borderContact(player)) {
+            player.moveLeft()
+        }
     }
 
     if (moveUp) {
         clearScreen()
         player.moveUp()
+        if (borderContact(player)) {
+            player.moveDown()
+        }
     }
 
     if (moveDown) {
         clearScreen()
         player.moveDown()
+        if (borderContact(player)) {
+            player.moveUp()
+        }
     }
 
     rock.draw()
+    rock2.draw()
+    rock3.draw()
+    rock4.draw()
+    rock5.draw()
+    rock6.draw()
     player.draw()
 
     requestAnimationFrame(gameLoop)
